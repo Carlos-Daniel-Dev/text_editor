c88e53777b493f57a3bb780554ab637e797d97d8
CarlosDaniel <105173667+Carlos-Daniel-Dev@users.noreply.github.com>
2024-02-21 20:54:21 -0300

    now you can use backspace in crunch

--- src/screen.c
+++ src/screen.c
@@ -5,18 +5,28 @@ void type(char character)
 	printw("%c", character);
 }
 
-void start_screan()
+void untype()
 {
-	initscr();
-	cbreak();
-	noecho();
-	keypad(stdscr, TRUE);
-	printw("Crunch started\n");
+	printw("\b \b");
+}
+
+int start_screan()
+{
+    initscr();
+    cbreak();
+    noecho();
+    keypad(stdscr, TRUE);
+    printw("Crunch started\n");
 
     int character;
     while ((character = getch()) != 'q') {
-        type(character);
+        if (character == KEY_BACKSPACE || character == 127) 
+            untype();
+        else
+            type(character);
     }
 
     endwin();
+
+    return 0;
 }
